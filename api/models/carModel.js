0f495193c96616cb3c6bcdf526d0a9f5cc5cc0be
Richard Paez <Richardp809@gmail.com>
2017-08-13 18:18:53 -0400

    Expanded uppon the car model to further describe a vehicle

--- api/models/carModel.js
+++ api/models/carModel.js
@@ -6,15 +6,30 @@ var Schema = mongoose.Schema;
 var carSchema = new Schema({
   make: {
     type: String,
-    Required: 'Please supply the make'
+    Required: 'Please supply make'
   },
   model: {
       type: String,
-      Reqired: 'Please supply the model'
+      Reqired: 'Please supply model'
   },
   year: {
     type: Number,
-    Requried: 'Please supply the model year'
+    Requried: 'Please supply model year'
+  },
+  mileage: {
+    type: Number,
+    Required: 'Please supply vehicle mileage'
+  },
+  color: {
+    type: Number,
+    Required: 'Please supply the vehicle color'
+  },
+  trim: {
+    type: String
+  },
+  hidden: {
+    type: Boolean,
+    default: false
   },
   Created_date: {
     type: Date,

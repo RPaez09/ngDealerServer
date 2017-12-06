424db295e0cb00ab8107a3a7e4370fd032e48bdc
Richard Paez <Richardp809@gmail.com>
2017-12-06 14:54:42 -0500

    updated strategy for a jwt strategy

--- config/passport.js
+++ config/passport.js
@@ -1,53 +1,22 @@
-var LocalStrategy = require('passport-local').Strategy;
-
+var config = require('../config/database');
+var ExtractJwt = require('passport-jwt').ExtractJwt;
+var JwtStrategy = require('passport-jwt').Strategy
 var User = require('./api/models/userModel');
 
 module.exports = function( passport ){
-
-    passport.serializeUser( function( user , done ) {
-        done( null , user.id );
-    } );
-
-    passport.deserializeUser( function( id , done ) {
-        User.findById( id , function( err , user ){
-            done( err , user );
-        } );
-    } );
-
-    passport.use( 'local-signup' , new LocalStrategy({
-        
-        usernameField: 'email',
-        passwordField: 'password',
-        passReqToCallback: true
-    },
-
-    function( req , email , password , done ){
-
-        process.nextTick( function(){
-
-            User.findOne( { 'local.email' : email } , function( err , user ){
-                if( err ){
-                    return done( err )
-                }
-
-                if( user ) {
-                    return done( null , false , req.flash( 'signupMessage' , 'That email is already taken.' ) )
-                }
-
-                else {
-                    var newUser = new User();
-
-                    newUser.local.email = email;
-                    newUser.local.password = newUser.generateHash(password);
-
-                    newUser.save( function(err){
-                        if(err) {
-                            throw err
-                        }
-                        return done(null , newUser)
-                    });
-                }
-            });
-        });
-    }));
+    var opts = {};
+    // opts.jwtFromRequest = ExtractJwt.fromAuthHeader();  //old
+    opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('JWT');
+    opts.secretOrKey = config.secret;
+    passport.use (
+        new JwtStrategy ( opts , function( jwt_payload, done ) 
+            {
+                User.findOne( { id: jwt_payload.id } , function( err , user ) 
+                {
+                    if ( err ) { return done( err , false ); }
+                    if ( user ) { done( null, user ); }
+                    else { done( null, false ); }
+                } );
+            }
+    ));
 };

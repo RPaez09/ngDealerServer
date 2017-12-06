2372532d01f8435690a8894e4b03695e5799c42c
Richard Paez <Richardp809@gmail.com>
2017-12-06 17:35:54 -0500

    Token extraction in carController

--- api/controllers/carController.js
+++ api/controllers/carController.js
@@ -1,5 +1,19 @@
 'use strict';
 
+function getToken( headers ){
+  if( headers && headers.authorization ){
+      var parted = headers.authorization.split(' ');
+      if( parted.length === 2 ) {
+          return parted[1];
+      }
+      else {
+          return null;
+      }
+  } else {
+      return null;
+  }
+}
+
 var mongoose = require('mongoose'),
   Car = mongoose.model('Cars');
 

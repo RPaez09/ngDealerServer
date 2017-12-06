871821a15d2842e6e75751a6eeb10ce5f3bad87e
Richard Paez <Richardp809@gmail.com>
2017-12-06 14:42:39 -0500

    Cleanup of requirements and activation of passportjs

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 var bodyParser  = require('body-parser');
 var cookieParser= require('cookie-parser');
-var DBconfig    = require('./config/database.js');
 var express     = require('express');
+var config      = require('./config/database.js');
 var mongoose    = require('mongoose');
 var morgan      = require('morgan');
 var passport    = require('passport');
 var routes      = require('./api/routes/Routes');
 
 var app = express();
-
 var port = process.env.PORT || 3000;
 
+//DB setup
 mongoose.Promise = global.Promise;
-mongoose.connect(DBconfig.url);
+mongoose.connect(config.url);
 
 app.use(morgan('dev'));
 app.use(cookieParser());
@@ -24,6 +24,8 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
+
+app.use(passport.initialize());
 
 routes(app);
 

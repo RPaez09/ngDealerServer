9f070750ff393641fb9f7a1e624bcb95b564e310
Richard Paez <Richardp809@gmail.com>
2017-12-06 14:51:38 -0500

    Updated user model

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,38 +1,53 @@
-var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
+var mongoose = require('mongoose');
+
 var Schema = mongoose.Schema;
 
-var userSchema = new  Schema({
-    local : {
-        firstName : {
+var UserSchema = new Schema(
+    {
+        username: 
+        { 
             type: String,
-            Required: 'Please supply a first name'
+            unique: true,
+            required: true
         },
-        lastName : {
+        password: 
+        {
             type: String,
-            Required: 'Please supply a last name'
-        },
-        email : {
-            type : String,
-            Required : 'Please supply a user email'
-        },
-        password : {
-            type : String,
-            Required: 'Please supply a user password'
-        },
-        isAdmin: {
-            type: Boolean,
-            default: false
+            required: true
         }
+    });
+
+UserSchema.pre( 'save' , function(next) {
+
+    var user = this;
+
+    if( this.isModified('password') || this.isNew ){
+        bcrypt.genSalt( 10 , function( err , salt ){
+            if( err ){
+                return next(err);
+            }
+            bcrypt.hash( user.password, salt, null, function( err , hash ) {
+                if( err ){
+                    return next(err);
+                }
+                user.password = hash;
+                next();
+            });
+        });
+    }
+    else{
+        return next();
     }
 });
 
-userSchema.methods.generateHash = function( password ) {
-    return bcrypt.hashSync( password , bcrypt.genSaltSync(8) , null );
+UserSchema.methods.comparePassword = function( passw, cb ){
+    bcrypt.compare( passw, this.password, function(err, isMatch) {
+        if(err) {
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
 };
 
-userSchema.methods.validPassword = function( password ){
-    return bcrypt.compareSync( password , this.local.password );
-};
-
-module.exports = mongoose.model( 'User' , userSchema );
+module.exports = mongoose.model('User' , UserSchema);
